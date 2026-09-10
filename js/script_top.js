2e24033583b29025c98a1d57d0a3e14eb3601849
test-taro-and-ziro <boa_sorte_1175@yahoo.co.jp>
2026-09-10 23:51:59 +0900

    Update script_top.js

--- js/script_top.js
+++ js/script_top.js
@@ -1,30 +1,5 @@
-// バージョン（例: 10.13.0）や拡張子（.js）まで正しく記述します
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js';
-import { 
-  getFirestore, 
-  collection, // データI/Oで使用する大きい箱
-  doc,    // データI/Oで使用する小さい箱
-  addDoc, // 場所を自動生成させてデータを保存
-  getDocs, // すべてのデータを読み込む
-  setDoc, // 指定した場所にデータを書き込む
-  getDoc, // 指定した場所のデータを読み込む
-  updateDoc,  // 更新機能
-  deleteDoc  // 削除機能
-} from 'https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js';
-
-// ご自身のFirebaseプロジェクトの設定値
-const firebaseConfig = {
-    apiKey: "AIzaSyBUfZOKhw9nHc5K6sq7pMWH2eeVixVx3wI",
-    authDomain: "quiz-battle-b8b48.firebaseapp.com",
-    projectId: "quiz-battle-b8b48",
-    storageBucket: "quiz-battle-b8b48.firebasestorage.app",
-    messagingSenderId: "883874950005",
-    appId: "1:883874950005:web:08b22a374ccb1e6133013c"
-};
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// Initialize Cloud Firestore
-const db = getFirestore(app);
+// 💡 共通設定ファイルから db を読み込む
+import { db } from './firebase-config.js';
 
 const ADMIN_PASSWORD = "admin1234"; 
 
