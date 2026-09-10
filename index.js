cf58f09c01df22c1aef825957d66c479ce62e1bc
test-taro-and-ziro <boa_sorte_1175@yahoo.co.jp>
2026-09-10 16:41:29 +0900

    Comment out Firebase initialization imports
    Comment out Firebase imports in index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "https://gstatic.com";
-import { getFirestore, collection, addDoc } from "https://gstatic.com";
+//import { initializeApp } from "https://gstatic.com";
+//import { getFirestore, collection, addDoc } from "https://gstatic.com";
 
 // ご自身のFirebaseプロジェクトの設定値
 const firebaseConfig = {
