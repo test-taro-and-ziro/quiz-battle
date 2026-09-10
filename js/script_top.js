a00b343eef50c38eae8a363dabe5c65272d099d8
test-taro-and-ziro <boa_sorte_1175@yahoo.co.jp>
2026-09-10 23:52:59 +0900

    Update script_top.js

--- js/script_top.js
+++ js/script_top.js
@@ -1,5 +1,16 @@
 // 💡 共通設定ファイルから db を読み込む
 import { db } from './firebase-config.js';
+import { 
+  getFirestore, 
+  collection, // データI/Oで使用する大きい箱
+  doc,    // データI/Oで使用する小さい箱
+  addDoc, // 場所を自動生成させてデータを保存
+  getDocs, // すべてのデータを読み込む
+  setDoc, // 指定した場所にデータを書き込む
+  getDoc, // 指定した場所のデータを読み込む
+  updateDoc,  // 更新機能
+  deleteDoc  // 削除機能
+} from 'https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js';
 
 const ADMIN_PASSWORD = "admin1234"; 
 
