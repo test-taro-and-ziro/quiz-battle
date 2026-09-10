d55fb1e333f990deaa4f1c6eb98b276373d3872e
test-taro-and-ziro <boa_sorte_1175@yahoo.co.jp>
2026-09-10 17:36:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // バージョン（例: 10.13.0）や拡張子（.js）まで正しく記述します
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js';
-import { getFirestore, collection, addDoc, getDocs } from 'https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js';
+import { 
+  getFirestore, 
+  collection, // データI/Oで使用する大きい箱
+  doc,    // データI/Oで使用する小さい箱
+  addDoc, // 場所を自動生成させてデータを保存
+  getDocs, // すべてのデータを読み込む
+  setDoc, // 指定した場所にデータを書き込む
+  getDoc  // 指定した場所のデータを読み込む
+} from 'https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js';
 
 // ご自身のFirebaseプロジェクトの設定値
 const firebaseConfig = {
