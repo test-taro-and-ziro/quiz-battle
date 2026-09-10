ec15bc53debbc1716cc372640356101a63ca8747
test-taro-and-ziro <boa_sorte_1175@yahoo.co.jp>
2026-09-10 15:41:44 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,12 @@ import { getFirestore, collection, doc, setDoc, getDoc, getDocs, updateDoc } fro
 // ご自身のFirebaseプロジェクトの設定値
 const firebaseConfig = {
     apiKey: "AIzaSyBUfZOKhw9nHc5K6sq7pMWH2eeVixVx3wI",
-    authDomain: "quiz-battle-b8b48.firebaseapp.com",
+    authDomain: "://firebaseapp.com",
     projectId: "quiz-battle-b8b48",
     storageBucket: "quiz-battle-b8b48.firebasestorage.app",
     messagingSenderId: "883874950005",
     appId: "1:883874950005:web:08b22a374ccb1e6133013c"
 };
-
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
