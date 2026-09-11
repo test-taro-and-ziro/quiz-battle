61f97c8bf9fc1e8de36dfe0746920072979ec458
test-taro-and-ziro <boa_sorte_1175@yahoo.co.jp>
2026-09-11 14:15:35 +0900

    Update script_quest.js

--- js/script_quest.js
+++ js/script_quest.js
@@ -1,10 +1,18 @@
 // ==========================================
 // クエスト選択画面（quest.html）専用プログラム
 // ==========================================
-
-// 共通設定ファイルから db を読み込む
 import { db } from './firebase-config.js';
-import { doc, getDoc } from 'https://gstatic.com';
+import { 
+  getFirestore, 
+  collection, // データI/Oで使用する大きい箱
+  doc,    // データI/Oで使用する小さい箱
+  addDoc, // 場所を自動生成させてデータを保存
+  getDocs, // すべてのデータを読み込む
+  setDoc, // 指定した場所にデータを書き込む
+  getDoc, // 指定した場所のデータを読み込む
+  updateDoc,  // 更新機能
+  deleteDoc  // 削除機能
+} from 'https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js';
 
 let currentUser = null;
 
