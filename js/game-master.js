9b4a5db732f90ec2bd2ab037be6c3b634510deca
test-taro-and-ziro <boa_sorte_1175@yahoo.co.jp>
2026-09-14 11:14:14 +0900

    Update game-master.js

--- js/game-master.js
+++ js/game-master.js
@@ -2,18 +2,7 @@
 // アニマルクエスト：ゲーム全体共通マスタ管理ファイル
 // ==========================================
 // 💡 共通設定ファイルから db を読み込む
-import { db } from './firebase-config.js';
-import { 
-  getFirestore, 
-  collection, // データI/Oで使用する大きい箱
-  doc,    // データI/Oで使用する小さい箱
-  addDoc, // 場所を自動生成させてデータを保存
-  getDocs, // すべてのデータを読み込む
-  setDoc, // 指定した場所にデータを書き込む
-  getDoc, // 指定した場所のデータを読み込む
-  query,  // フィールド検索に必要
-  where, //　フィールド検索に必要
-} from 'https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js';
+import { db, collection, doc, addDoc, getDocs, setDoc, getDoc, updateDoc, deleteDoc, query, where, orderBy } from './firebase-config.js';
 
 // 各画面で共有するための変数
 export let animalMasterData = [];     // 💡 動物マスタ
