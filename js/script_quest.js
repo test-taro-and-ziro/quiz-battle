f0ce69390244dea696ba929b9f807583f6cfead1
test-taro-and-ziro <boa_sorte_1175@yahoo.co.jp>
2026-09-15 20:56:30 +0900

    Fix typo in setting quest norma text content

--- js/script_quest.js
+++ js/script_quest.js
@@ -174,7 +174,7 @@ function selectQuest(dungeon) {
     // ダンジョン名を表示
     document.getElementById('selected-quest-name').textContent = dungeon.name;
     // 💡 データベースから読み込んだノルマ（norma）の値をモーダルにセット！
-    document.getElementById('selected-quest-norma').textContent = dungeon.norma";
+    document.getElementById('selected-quest-norma').textContent = dungeon.norma;
     
     const genreContainer = document.getElementById('genre-list-container');
     genreContainer.innerHTML = ''; 
